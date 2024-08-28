4382df2137749e47e452dbc520a0bbe8235c53a2
shdab <130516923+shadatr@users.noreply.github.com>
2024-08-28 14:15:40 +0300

    first commit

--- src/main/java/com/example/university_managment_system/UniversityManagmentSystemApplication.java
+++ src/main/java/com/example/university_managment_system/UniversityManagmentSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UniversityManagmentSystemApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(UniversityManagmentSystemApplication.class, args);
 	}
