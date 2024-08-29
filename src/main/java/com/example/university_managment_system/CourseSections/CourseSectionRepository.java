dce74312de7ab4f166d88b877d9ccb732e8737bb
shdab <130516923+shadatr@users.noreply.github.com>
2024-08-29 21:47:32 +0300

    .

--- src/main/java/com/example/university_managment_system/CourseSections/CourseSectionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.university_managment_system.CourseSections;
-import com.example.university_managment_system.major.Major;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseSectionRepository extends JpaRepository<CourseSection, Long> {
-}
