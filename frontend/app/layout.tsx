0f78ed60948d643b5ed8c4b83acbb2421b4ee589
shada <shadadaab@gmail.com>
2024-09-08 20:46:33 +0300

    path error fixed

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,14 +7,16 @@ import { SessionProvider } from "next-auth/react";
 
 export default function RootLayout({
   children,
+  session,
 }: Readonly<{
   children: React.ReactNode;
+  session: any;
 }>) {
   return (
     <html lang="en">
       <body className={cn("min-h-screen bg-background font-sans antialiased")}>
         <NextUIProvider>
-          <SessionProvider >
+          <SessionProvider session={session}>
 
           {children}
           </SessionProvider>
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,8 +2,8 @@ module.exports = {
     async rewrites() {
       return [
         {
-          source: '/api/:path*',
-          destination: 'http://18.196.61.213:8080/api/:path*', // Proxy to Backend
+          source: '/api/v1/:path*',
+          destination: 'http://18.196.61.213:8080/api/v1/:path*', // Proxy to Backend
         },
       ]
     },
