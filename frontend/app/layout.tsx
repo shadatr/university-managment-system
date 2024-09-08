d3e09f743ffe4dbc90928d4cdd8fc412ad6180f0
shada <shadadaab@gmail.com>
2024-09-08 21:45:50 +0300

    Auth Context to wrap the layout added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,24 +4,22 @@ import { cn } from "@/lib/utils";
 import { NextUIProvider } from "@nextui-org/react";
 import { Toaster } from "sonner";
 import { SessionProvider } from "next-auth/react";
-import { Session } from "next-auth";
+import { Session, getServerSession } from "next-auth";
+import AuthContext from "@/components/authContext";
 
-export default function RootLayout({
-  children,
-  params: { session, ...params },
-}: {
+export interface AccountLayoutProps {
   children: React.ReactNode;
-  params: { session?: Session };  // Adjust the type of session if necessary
-}) {
+}
+export default function RootLayout({ children }: AccountLayoutProps) {
   return (
     <html lang="en">
       <body className={cn("min-h-screen bg-background font-sans antialiased")}>
-        <NextUIProvider>
-          <SessionProvider session={session}>
+        <AuthContext>
+          <NextUIProvider>
             {children}
-          </SessionProvider>
-          <Toaster position="bottom-right" richColors />
-        </NextUIProvider>
+            <Toaster position="bottom-right" richColors />
+          </NextUIProvider>
+        </AuthContext>
       </body>
     </html>
   );
